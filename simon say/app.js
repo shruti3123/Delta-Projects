3a52870bb4ee0e245f2fa669b815b9b93568bbc1
shruti panwar <Shrutipanwar312@gmail.com>
2025-08-05 19:59:52 +0530

    add

---
+++ simon say/app.js
@@ -0,0 +1,98 @@
+let gameseq = []; 
+let userseq = [];
+
+let btns = ["yellow","red","purple","green"];
+
+let started = false;
+ let level = 0;
+ let haigscore = 0;
+
+let h2 = document.querySelector("h2");
+
+  document.addEventListener("keypress", function(){
+    if(started == false){
+        console.log("game is started");
+        started = true;
+
+        levelUp();
+    }
+ });
+
+
+  function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function (){
+        btn.classList.remove("flash");
+      },250);
+  }
+
+  function userFlash(btn) {
+    btn.classList.add("userFlash");
+    setTimeout(function (){
+        btn.classList.remove("userFlash");
+      },250);
+  }
+
+
+ function levelUp(){
+    userseq = [];
+    level++;
+    h2.innerText = `level ${level}`;
+    
+    if(haigscore<=level){
+        haigscore=level;
+    }else if(levelUp == haigscore){
+        console.log(haigscore)
+    }else{
+        
+    }
+   
+    let randInx = Math.floor(Math.random()* 4);
+    let randColor = btns[randInx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    
+    gameseq.push(randColor);
+    console.log(gameseq)
+    gameFlash(randBtn);
+
+ }
+
+ function checkAns(idx){
+    if(userseq[idx] === gameseq[idx]){
+        if(userseq.length == gameseq.length){
+            setTimeout(levelUp, 1000);
+        }
+        console.log("same value");
+    } else {
+     h2.innerHTML = `game over! Your score was <b>${level}</b><br>and Your haigh score is <b>${haigscore}</b> <br>Press any key to start.`;
+     document.querySelector("body").style.background = "red";
+     setTimeout(function(){
+     document.querySelector("body").style.background = "white";
+     },150);
+     reset();
+      
+    }
+ }
+
+function btnPress(){
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userseq.push(userColor);
+
+    checkAns(userseq.length-1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    started =false;
+    gameseq =[];
+    userseq = [];
+    level = 0;
+}
